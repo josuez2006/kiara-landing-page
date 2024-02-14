3563ef58d72965908b6a2a217d437e47dcc2aa23
Josue Zalazar <josue.zalazar20066@gmail.com>
2024-02-14 17:41:11 -0300

    Adding interactivity to the class structure; fixing spacing on the hero; and fixing navbar issue on mobile

--- src/Components/ClassStructure/index.jsx
+++ src/Components/ClassStructure/index.jsx
@@ -1,21 +1,51 @@
+import { useEffect, useState } from "react"
+
 function ClassStructure() {
+  const [firstSelected, setFirstSelected] = useState(true)
+  const [secondSelected, setSecondSelected] = useState(false)
+  const [thirdSelected, setThirdSelected] = useState(false)
+
+  useEffect(() => {
+    let myInterval =
+    setInterval(() => {
+      if (firstSelected) {
+        setSecondSelected(true)
+        setFirstSelected(false)
+      } 
+      
+      else if (secondSelected) {
+        setThirdSelected(true)
+        setSecondSelected(false)
+      }
+
+      else if (thirdSelected) {
+        setFirstSelected(true)
+        setThirdSelected(false)
+      }
+
+    }, 1500)
+
+    return () => {
+      clearInterval(myInterval)
+    }
+  })
   return (
     <section className="section bg-secondary | class-structure">
         <h2 className="title">¿Cómo funcionan las clases?</h2>
 
         <div className="cards">
           <div className="rounded bg-primary | card">
-            <div className="number heading rounded">1</div>
+            <div className="number heading rounded" aria-selected={firstSelected}>1</div>
             <h3 className="subtitle">Despertamos la curiosidad</h3>
           </div>
 
           <div className="rounded bg-primary | card">
-            <div className="number heading rounded">2</div>
+            <div className="number heading rounded"aria-selected={secondSelected}>2</div>
             <h3 className="subtitle">Estudiamos la teoría</h3>
           </div>
 
           <div className="rounded bg-primary | card">
-            <div className="number heading rounded" aria-active='true'>3</div>
+            <div className="number heading rounded" aria-selected={thirdSelected}>3</div>
             <h3 className="subtitle">Jugamos y aprendemos</h3>
           </div>
         </div>
--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -4,8 +4,10 @@ function Navbar() {
   return (
     <nav className="nav bg-primary">
         <span className="logo">Profe Kiara</span>
-        <button className="btn-icon bg-secondary"><img src="./assets/icons/Phone.svg" alt="" /></button>
-        <a href="https://wa.me/5491168492899"><button className="btn-secondary btn-text">Reservá una clase</button></a>
+        <a href="https://wa.me/5491168492899">
+          <button className="btn-secondary btn-text">Reservá una clase</button>
+          <button className="btn-icon bg-secondary"><img src="./assets/icons/Phone.svg" alt="" /></button>
+        </a>
     </nav>
   )
 }
